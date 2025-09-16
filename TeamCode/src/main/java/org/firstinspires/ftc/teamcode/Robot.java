bb45e23ff731d38cb0f615ebd04f8875925e4259
Sam DeKoninck <>
2025-09-16 15:19:07 -0400

    Pedro template

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+
+public class Robot {
+    Follower follower;
+
+    public Robot() {
+        follower = Constants.createFollower();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestTeleOp.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@TeleOp(name="TeleOp", group="Competition")
+public class TestTeleOp extends OpMode {
+    private Robot robot;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
