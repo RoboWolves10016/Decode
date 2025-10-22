91ebc9e2edd02a967491f5180f11ba3598a2a334
asluchenba15 <61923115+asluchenba15@users.noreply.github.com>
2025-10-21 20:55:45 -0400

    Tuned PIDF and fixed rpm inputs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Launcher.java
@@ -36,11 +36,11 @@ public class Launcher extends Subsystem {
     private double currentRpm = 0;
     private double distanceToGoal = 0;
 
-    public static double kP = 0.00003;
-    public static double kI = 200.0;
-    public static double kD = 0.000001;
-    public static double kS = 0;
-    public static double kV = 0.00019;
+    public static double kP = 0.002;
+    public static double kI = 0.005;
+    public static double kD = 0;
+    public static double kS = 0.13;
+    public static double kV = 0.0002;
     public static double kA = 0;
 
     public Launcher(HardwareMap hwMap) {
@@ -53,9 +53,9 @@ public class Launcher extends Subsystem {
         motor = new MotorEx(hwMap, "Launcher", Motor.GoBILDA.BARE);
         motor.setInverted(true);
         motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        motor.setRunMode(Motor.RunMode.VelocityControl);
-        motor.setVeloCoefficients(kP, kI, kD);
-        motor.setFeedforwardCoefficients(0, kV, 0);
+        motor.setRunMode(Motor.RunMode.RawPower);
+//        motor.setVeloCoefficients(kP, kI, kD);
+//        motor.setFeedforwardCoefficients(0, kV, 0);
         velocityController.setPIDF(kP, kI, kD, 0);
         feedforward = new SimpleMotorFeedforward(kS, kV, kA);
 
@@ -68,12 +68,12 @@ public class Launcher extends Subsystem {
         distanceToGoal = robotState.getVectorToGoal().getMagnitude();
 //        targetRpm = distanceToRpm(distanceToGoal);
         currentRpm = (motor.getCorrectedVelocity() / 28) * 60;
-        motor.set(targetRpm);
+//        motor.set(targetRpm);
 
         velocityController.setPIDF(kP, kI, kD, 0);
         feedforward = new SimpleMotorFeedforward(kS, kV, kA);
 
-        motor.set(velocityController.calculate(currentRpm, targetRpm) + feedforward.calculate(currentRpm, motor.getAcceleration()));
+        motor.set(Math.max(0,velocityController.calculate(currentRpm, targetRpm) + feedforward.calculate(targetRpm, motor.getAcceleration())));
         updateTelemetry();
     }
 
